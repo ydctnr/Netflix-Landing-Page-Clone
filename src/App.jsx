8584fc7e7bdfa28e4664c86836b345e69bf8a3e1
ydctnr <152324574+ydctnr@users.noreply.github.com>
2025-01-17 16:03:47 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import React, { useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-
 function App() {
   const { i18n } = useTranslation();
   const [language, setLanguage] = useState('en');
@@ -15,23 +14,16 @@ function App() {
     i18n.changeLanguage(newLanguage);
   };
 
-
   return (
     <Router>
       <div className='w-full flex flex-col justify-center items-center overflow-x-hidden'>
-
         <Routes>
-
-        <Route path="/" element={<HomePage language={language} onLanguageChange={handleLanguageChange}/>} />
-      
-        <Route path="/login" element={<LoginPage language={language} onLanguageChange={handleLanguageChange}/>} />
-              
-
+          <Route path="/" element={<HomePage language={language} onLanguageChange={handleLanguageChange} />} />
+          <Route path="/#login" element={<LoginPage language={language} onLanguageChange={handleLanguageChange} />} />
         </Routes>
-
       </div>
     </Router>
-  )
+  );
 }
 
-export default App 
+export default App;
